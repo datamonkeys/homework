a4ba4a7d01e7dcf9613bf88d0a736cb1d3dd5526
Markus Doppelreiter <markusdoppelreiter@Markuss-MacBook-Pro-3.local>
2019-12-09 20:52:37 +0100

    reviewed

--- src/App.js
+++ src/App.js
@@ -4,31 +4,18 @@ import './App.css';
 
 
 class Homework extends React.Component {
+  formatName() {
+    return this.props.user.firstName + " " + this.props.user.lastName; //img it is not working
+  }
   render() {
-     const formatName =(user)=> {
-      return user.firstName + ' ' + user.lastName; //img it is not working
-    }
-
-    const user = {
-      firstName: 'Ruba',
-      lastName: 'Abu Isa',
-      avatarUrl: 'https://www.w3schools.com/howto/img_avatar2.png'
-    };
-
-
-    const getGreeting = (user) => {
-        if(user) {
-          return <h1>Hello, {formatName(user)}!</h1>;
-        }
-        return <h1>Hello, strenger</h1>;
-      }
-
-    const element =getGreeting(user);
-   
-    return element;
+    return (
+      <div id="result">
+        <img src={this.props.user.avatarUrl} alt={this.formatName()} />
+        <h1>Hello, {this.formatName()}!</h1>;
+      </div>
+    );
   }
 }
-
 
 export default Homework;
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Homework from './App.js';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import Homework from "./App.js";
+import "./index.css";
 
+const user = {
+  firstName: "Ruba",
+  lastName: "Abu Isa",
+  avatarUrl: "https://www.w3schools.com/howto/img_avatar2.png"
+};
 
-ReactDOM.render(<Homework />, document.getElementById('root'));
-
+ReactDOM.render(<Homework user={user} />, document.getElementById("root"));
